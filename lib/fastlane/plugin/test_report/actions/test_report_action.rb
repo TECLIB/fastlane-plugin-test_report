5778411b7729fc6e3ee974bf25d074010b8a5835
Naylin Medina <nmedina@teclib.com>
2018-09-28 10:10:56 -0400

    feat(parse): add xml parse
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- lib/fastlane/plugin/test_report/actions/test_report_action.rb
+++ lib/fastlane/plugin/test_report/actions/test_report_action.rb
@@ -1,15 +1,60 @@
 require 'fastlane/action'
 require_relative '../helper/test_report_helper'
+require "erb"
+require "nokogiri"
 
 module Fastlane
   module Actions
     class TestReportAction < Action
       def self.run(params)
         UI.message("The test_report plugin is working!")
+
+        report = File.read(File.expand_path(params[:path]))
+        doc = Nokogiri::XML(File.open(report))
+        test_suite_name = @doc.xpath('//test_suite').attr("name")
+
+        template = '---
+        layout: testReport
+        ---
+        
+        <div class="total row">
+    <div class="col-sm-12">
+        <h2>Test Results</h2>
+    </div>
+    <div class="col-sm-12">
+        <h2> 8 tests</h2>
+    </div>
+</div>
+<% test_suite_name.each do |name| %>
+<div>
+    <div class="test-suite row failing" onclick="changeDisplay('test-1')">
+        <h4 class="col-sm-12"><%= name %></h4>
+        <h4 class="col-sm-12">1 tests</h4>
+    </div>
+<% end %>
+    <div id="test-1">
+
+        <div class="failing row">
+            <div class="title col-sm-12 dropdown-item">
+                <p>testTakeScreenshots</p>
+            </div>
+            <div class="time col-sm-12 dropdown-item">
+                <p>29.457s</p>
+            </div>
+        </div>
+    </div>
+</div>'
+
+        result = ERB.new(template).result(binding())
+
+        open('test-report/index.html', 'w') do |f|
+          f.puts result
+        end
+
       end
 
       def self.description
-        "customized template for test reports[D[D[D[D[D[D[D[D[D[D[D"
+        "Create customized HTML template for test reports"
       end
 
       def self.authors
@@ -27,11 +72,10 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "TEST_REPORT_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :path,
+                                  env_name: "TEST_REPORT_PATH",
+                               description: "Path to the test report",
+                             default_value: './fastlane/test_output/report.xml')
         ]
       end
 
