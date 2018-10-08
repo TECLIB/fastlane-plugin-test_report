4780542deaa0c7c6134263922c226601b1d1509b
Naylin <nmedina@teclib.com>
2018-10-08 18:29:39 -0400

    test(report): add unit tests
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- spec/test_report_action_spec.rb
+++ spec/test_report_action_spec.rb
@@ -5,5 +5,40 @@ describe Fastlane::Actions::TestReportAction do
 
       Fastlane::Actions::TestReportAction.run(nil)
     end
+
+    it 'parse xml with default template' do
+      ENV['TEST_REPORT_PATH'] = "../spec/fixture/report.xml"
+      ENV['OUTPUT_DIR'] = "../spec/fixture"
+
+      run = Fastlane::FastFile.new.parse("lane :test do
+        test_report
+      end").runner.execute(:test)
+
+      a = File.read("./spec/fixture/result.html")
+      b = File.read("./spec/fixture/index.html")
+
+      File.delete("./spec/fixture/index.html")
+      ENV.delete('TEST_REPORT_PATH')
+      ENV.delete('OUTPUT_DIR')
+    end
+
+    it 'parse xml with custom template' do
+      ENV['TEST_REPORT_PATH'] = '../spec/fixture/report.xml'
+      ENV['TEMPLATE_PATH'] = "../spec/fixture/template.erb"
+      ENV['OUTPUT_DIR'] = '../spec/fixture'
+
+      run = Fastlane::FastFile.new.parse("lane :test do
+        test_report
+      end").runner.execute(:test)
+
+      a = File.read("./spec/fixture/template.html")
+      b = File.read("./spec/fixture/index.html")
+
+      expect(a).to eq(b)
+
+      ENV.delete('TEST_REPORT_PATH')
+      ENV.delete('TEMPLATE_PATH')
+      ENV.delete('OUTPUT_DIR')
+    end
   end
 end
