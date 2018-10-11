fbcdfecd880a82cfb61836bebacbe5579280e19f
Naylin Medina <nmedina@teclib.com>
2018-10-11 16:40:37 -0400

    test(spec): remove example test
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- spec/test_report_action_spec.rb
+++ spec/test_report_action_spec.rb
@@ -1,10 +1,5 @@
 describe Fastlane::Actions::TestReportAction do
   describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The test_report plugin is working!")
-
-      Fastlane::Actions::TestReportAction.run(nil)
-    end
 
     it 'parse xml with default template' do
       ENV['TEST_REPORT_PATH'] = "../spec/fixture/report.xml"
